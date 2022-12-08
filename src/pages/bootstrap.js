1aede182feb703e7a682a497f51fbf7fc5460988
jh <jeonghuns0128@naver.com>
2022-12-08 16:54:21 +0900

    공통 헤더 추가

--- src/pages/bootstrap.js
+++ src/pages/bootstrap.js
@@ -1,24 +1,14 @@
-import {
-  Navbar,
-  Container,
-  Offcanvas,
-  Nav,
-  NavDropdown,
-  Form,
-  Button,
-  Carousel,
-  ListGroup,
-  Card,
-} from 'react-bootstrap'
+import { Navbar,  Container, Offcanvas, Nav, NavDropdown, Form, Button, Carousel, ListGroup, Card, CardGroup,Row, Col} from 'react-bootstrap'
 
 import { CiTextAlignJustify } from 'react-icons/ci'
 import { GiAmpleDress } from 'react-icons/gi'
 import { FaDog } from 'react-icons/fa'
 import { GiClothes, GiConverseShoe } from 'react-icons/gi'
-import { BsGithub } from 'react-icons/bs'
+import { BsCart4 } from 'react-icons/bs'
 import { ImSearch } from 'react-icons/im'
 import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
+import { HistoryBack } from '../function/function'
 
 function Header() {
   return (
@@ -26,8 +16,11 @@ function Header() {
       <Navbar key={false} bg="light" expand={false} className="mb-3">
         <Container fluid>
           <Navbar.Brand>도그박</Navbar.Brand>
-          <ImSearch size="30" />
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+          <div>
+            <Navbar.Toggle style={{marginRight : "10px"}} aria-controls={`offcanvasNavbar-expand-${false}`} />
+            <ImSearch style={{marginRight : "10px"}} size="35" />
+            <BsCart4 style={{marginBottom : "5px"}} size="40" />
+          </div>
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${false}`}
             aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
@@ -102,7 +95,7 @@ function TopMenu() {
 
 function IndividualIntervals() {
   return (
-    <Carousel>
+    <Carousel style={{paddingTop : '10px'}}>
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100"
@@ -134,7 +127,7 @@ function IndividualIntervals() {
 function HorizontalCategory() {
   const navigate = useNavigate()
   return (
-    <ListGroup horizontal>
+    <ListGroup horizontal style={{paddingTop : '10px'}}>
       <ListGroup.Item>
         <CiTextAlignJustify size="40" /> 전체
       </ListGroup.Item>
@@ -154,14 +147,91 @@ function HorizontalCategory() {
   )
 }
 
-function CardList() {
-  return (
+function CommonHeader(){
+  return(
     <>
-      <Card style={{ width: '10' }}>
-        <Card.Img variant="top" src="./shoes1.jpg" />
-      </Card>
+      <div>
+        <HistoryBack />
+        
+          <ImSearch style={{marginLeft : "250px", marginRight : '10px'}} size="35" />
+          <BsCart4 style={{marginBottom : "5px"}} size="40" />
+        
+      </div>
     </>
   )
 }
 
-export { Header, TopMenu, IndividualIntervals, HorizontalCategory, CardList }
+function RecommendArea(){
+  return(
+    <Container style={{marginTop : '10px'}}>
+      <Row>
+        <Col style={{paddingLeft : '2px', paddingRight : '2px'}}>
+          <Card>
+            <Card.Img src="http://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_8311476%2F83114765936.jpg&type=f372_372" />
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '14px'}}>바잉몰</div>
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '12px'}}>[HONEST, YOU] 아..</div>
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '18px'}}>25,000</div>
+          </Card>
+        </Col>
+        <Col style={{paddingLeft : '2px', paddingRight : '2px'}}>
+          <Card>
+            <Card.Img src="http://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_8470200%2F84702007640.jpg&type=f372_372" />
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '14px'}}>트렌디어패럴</div>
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '12px'}}>♥주문폭주♥ mix..</div>
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '18px'}}>34,000</div>
+          </Card>
+        </Col>
+        <Col style={{paddingLeft : '2px', paddingRight : '2px'}}>
+          <Card>
+            <Card.Img src="http://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_8299192%2F82991926334.jpg&type=f372_372" />
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '14px'}}>어텀뮤트</div>
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '12px'}}>나이키브랜드</div>
+            <div style={{textAlign : 'left', paddingLeft : '4px', fontSize : '18px'}}>17,000</div>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+
+    
+    // <CardGroup style={{margin : 'auto'}}>
+      // <Card style={{height : '100px', width : '100px', margin : '10px'}}>
+      //   <Card.Img style={{height : '200px'}} src="http://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_8311476%2F83114765936.jpg&type=f372_372" />
+      //   <Card.Body>
+      //     <Card.Title>test</Card.Title>
+      //     <Card.Text>
+      //       test2
+      //     </Card.Text>
+      //   </Card.Body>
+      //   <Card.Footer>
+      //   <small className="text-muted">Last updated 3 mins ago</small>
+      //   </Card.Footer>
+      // </Card>
+    //   <Card style={{height : '100px', width : '100px'}}>
+    //     <Card.Img variant="top" style={{height : '200px'}} src="http://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_8311476%2F83114765936.jpg&type=f372_372" />
+    //     <Card.Body>
+    //       <Card.Title>test</Card.Title>
+    //       <Card.Text>
+    //         test2
+    //       </Card.Text>
+    //     </Card.Body>
+    //     <Card.Footer>
+    //     <small className="text-muted">Last updated 3 mins ago</small>
+    //     </Card.Footer>
+    //   </Card>
+    //   <Card style={{height : '100px', width : '100px'}}>
+    //     <Card.Img variant="top" src="http://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_8311476%2F83114765936.jpg&type=f372_372" />
+    //     <Card.Body>
+    //       <Card.Title>test</Card.Title>
+    //       <Card.Text>
+    //         test2
+    //       </Card.Text>
+    //     </Card.Body>
+    //     <Card.Footer>
+    //     <small className="text-muted">Last updated 3 mins ago</small>
+    //     </Card.Footer>
+    //   </Card>
+    // </CardGroup>
+  )
+}
+
+export { Header, TopMenu, IndividualIntervals, HorizontalCategory, CommonHeader, RecommendArea }
