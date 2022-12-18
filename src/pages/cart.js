654cd7bd3387f8658bca29880c65fc090cc63713
jh <jeonghuns0128@naver.com>
2022-12-18 20:59:52 +0900

    장바구니 전체선택, 개별선택 기능 구현

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react'
 import { BsCart4, BsCheckCircleFill } from 'react-icons/bs'
+import { FaRegCircle } from 'react-icons/fa'
+
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { HistoryBack } from '../function/function'
@@ -37,19 +40,78 @@ function CartHeader(){
 
   function CartDetail(){
     let cartList = useSelector((state)=>{return state.cart})
+    let [isSelectTot, setIsSelectTot] = useState(true)
+    let [isSelectOne, setIsSelectOne] = useState([])
+    let [listCnt, setListCnt] = useState(cartList.length)
 
+    useEffect(() => {
+      let copy = []
+      cartList.map((a,i)=>{
+        copy[i] = true
+        if(cartList.length === (i+1))
+          setIsSelectOne(copy)
+      })
+      console.log("tot!!!")
+    },[])
+
+    useEffect(() => {
+      let cnt = 0
+      console.log("listCnt : " + listCnt)
+      cartList.map((a,i) => {
+        if(isSelectOne[i] === true){
+          cnt = cnt +  1
+        }
+      })
+      console.log("cnt : " + cnt)
+      setListCnt(cnt)
+    },[isSelectOne])
+    
     return(
       <div>
-        <div style={{textAlign : 'left'}}><BsCheckCircleFill style={{marginLeft : '10px'}} size="20px" />전체선택 라인 <button style={{marginLeft : '150px'}}>상품삭제</button></div>
+        <div style={{textAlign : 'left', width : '100%', height : '20px'}}>
+          <div style={{width : '80%', float : 'left'}}>
+            {isSelectTot === true ? <BsCheckCircleFill style={{marginLeft : '10px', marginRight : '10px'}} size="20px" onClick={()=>{setIsSelectTot(false)}} /> : <FaRegCircle style={{marginLeft : '10px', marginRight : '10px'}} size="20px" onClick={()=>{setIsSelectTot(true)}}/>}
+            전체선택 ( {listCnt}/{cartList.length} )</div>
+          <div style={{width : '20%', float : 'left'}}><a onClick={()=>{alert('상품을 삭제하시겠습니까?')}}>상품삭제</a></div>
+        </div>
+        <hr style={{width : '100%', marginTop : '10px'}}/> 
         { 
         cartList.map((a,i)=>{
           return(
             <>
-              <div style={{textAlign : 'left'}}><BsCheckCircleFill style={{marginLeft : '10px'}} size="20px" />상품리스트 : {cartList[i].title}</div>
+              <div style={{textAlign : 'left', width : '100%'}}>
+                <div style={{width : '80%', float : 'left'}}> 
+                로라로라
+                </div>
+                <div style={{width : '20%', float : 'left'}}><a onClick={()=>{alert('쿠폰을 받으시겠습니까?')}}>쿠폰받기 </a></div>
+              </div>
+              
+              <hr style={{width : '100%', marginTop : '10px'}}/> 
+
+              <div style={{textAlign : 'left'}}>
+                {isSelectOne[i] === true ? <BsCheckCircleFill style={{marginLeft : '10px'}} size="20px" onClick={() => {
+                  let copy = [...isSelectOne]
+                  copy[i] = !isSelectOne[i]
+                  setIsSelectOne(copy)
+                }} /> : <FaRegCircle style={{marginLeft : '10px', marginRight : '10px'}} size="20px" onClick={()=>{
+                  let copy = [...isSelectOne]
+                  copy[i] = !isSelectOne[i]
+                  setIsSelectOne(copy)
+                }}/>}
+                 : {cartList[i].title}
+              </div>
               <div>결제금액 : {cartList[i].price} </div>
+              <button>바로구매</button> <button>상품 추가</button>
+              <hr/>
             </>
           )
-        })}
+        })
+        }
+        <div>
+          <div>총 상품금액</div>
+          <div>총 배송비</div>
+          <div>총 결제예상금액</div>
+        </div>
       </div>
     )
   }
