d26995af0947c15974b00fd2dd3b7ece8bdb3ec7
jh <jeonghuns0128@naver.com>
2023-02-01 17:52:19 +0900

    상품페이지 수정

--- src/pages/product.js
+++ src/pages/product.js
@@ -7,7 +7,7 @@ import { useNavigate, Route, Routes } from 'react-router-dom'
 import {changeCartList} from '../store/store.js'
 import { useDispatch, useSelector } from 'react-redux'
 import { RxShare2 } from 'react-icons/rx'
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar, AiOutlineCheck } from 'react-icons/ai'
 
 
 function ProductList(props) {
@@ -17,7 +17,7 @@ function ProductList(props) {
     const navigate = useNavigate()
 
     useEffect(() => {
-        axios.get(process.env.REACT_APP_API_URL + '/clothes/shoes/').then((result) => {
+        axios.get(process.env.REACT_APP_API_URL + '/product/clothes/shoes/').then((result) => {
         console.log('front : ' + result.data)
         setProduct(result.data)
         })
@@ -29,26 +29,67 @@ function ProductList(props) {
     console.log("product : " + product);
 
     return (
-        <div style={{marginTop : '20px'}}>
-            <CardGroup>
-                {product && product.map(product => {
-                    return(
-                    <Card onClick={() => {navigate('/clothes/shoes/' + product.id)}}>
-                        <Card.Img variant="top" style={{height : '200px'}} src={product.img_url} />
-                        <Card.Body>
-                        <Card.Title>{product.title}</Card.Title>
-                        <Card.Text>
-                            {product.content}
-                        </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    )
-                })}
-            </CardGroup>            
+        <>
+        <div style={{marginTop : '50px'}}>
+
         </div>
+        <div style={{ width : '100%'}}>
+            {product && product.map(product => {
+                return(
+                    <>
+                    <div style={{width : '100%', height : '170px', marginBottom : '20px'}} onClick={() => {navigate('/product/clothes/shoes/' + product.id)}}>
+                        <div style={{width : '150px', height : '170px', float : 'left'}}>
+                            <img src={product.img_url} style={{maxWidth:'100%', maxHeight:'100%'}}/>
+                        </div>
+                        <div style={{height : '150px', textAlign : 'left'}}>
+                            <div style={{paddingRight : '20px', marginBottom : '5px'}}>이소켈리 레드 리페어 세럼, 1개, 50ml</div>
+                            <div>
+                                <div style={{fontSize : '15px', color : 'red', float : 'left'}}>와우쿠폰할인가</div>
+                                <div style={{float : 'left'}}>&nbsp;13%&nbsp;</div>
+                                <div style={{textDecoration: 'line-through'}}>24,480원</div>
+                            </div>
+                            <div style={{marginBottom : '10px'}}>
+                                <div style={{fontSize : '20px', color : 'red', float : 'left'}}>21,100원 </div>
+                                <div style={{fontSize : '15px', color : 'red'}}>(10ml당 4,222원)</div>
+                            </div>
+                            <div style={{fontSize : '15px', color : 'green'}}>오늘(목) 도착 보장</div>
+                            <div style={{fontSize : '15px'}}>
+                                와우 혜택 적용 <AiOutlineCheck />
+                            </div>
+                            <div>
+                                <div style={{color : 'orange', float : 'left'}}><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /></div>
+                                <div>&nbsp;(10)</div>
+                            </div>
+                        </div>                       
+                    </div>
+                    <hr />
+                    </>
+                )
+            })}
+        </div>
+        </>
+
+
+        // <div style={{marginTop : '20px'}}>
+        //     <CardGroup>
+        //         {product && product.map(product => {
+        //             return(
+        //             <Card onClick={() => {navigate('/product/clothes/shoes/' + product.id)}}>
+        //                 <Card.Img variant="top" style={{height : '200px'}} src={product.img_url} />
+        //                 <Card.Body>
+        //                 <Card.Title>{product.title}</Card.Title>
+        //                 <Card.Text>
+        //                     {product.content}
+        //                 </Card.Text>
+        //                 </Card.Body>
+        //                 <Card.Footer>
+        //                 <small className="text-muted">Last updated 3 mins ago</small>
+        //                 </Card.Footer>
+        //             </Card>
+        //             )
+        //         })}
+        //     </CardGroup>            
+        // </div>
     )
 }
 
