cb5834d9756f4b452e384bd71192683650f5b704
jh <jeonghuns0128@naver.com>
2023-02-03 17:42:37 +0900

    상품 검색 api 추가

--- src/pages/search.js
+++ src/pages/search.js
@@ -9,7 +9,7 @@ import { HistoryBack } from '../function/function'
 import CloseButton from 'react-bootstrap/CloseButton';
 import axios from 'axios'
 import { searchList } from '../store/store'
-
+import { AiFillStar, AiOutlineCheck } from 'react-icons/ai'
 
 function SearchHeader(){
 
@@ -80,17 +80,47 @@ function SearchDetail() {
     let searchResult = useSelector((state)=>{return state.search})
     let searchResultCnt = searchResult[0].totcnt
     let searchResultList = searchResult[0].keyword
-
+    let navigate = useNavigate()
     return(
-        <><div style={{marginTop : '50px'}}>검색 결과 {searchResultCnt}건 있음!!!</div>
+        <>
+        <div style={{marginTop : '50px', textAlign : 'left', marginLeft : '10px', marginBottom : '10px'}}>검색 결과 : {searchResultCnt}건</div>
         
-        {searchResultList && searchResultList.map(searchResultList => {
-            return(
-                <div>{searchResultList._source.field}</div>
-            )  
-        })}
-
-        
+        <div style={{ width : '100%'}}>
+          {searchResultList && searchResultList.map(searchResultList => {
+                return(
+                    <>
+                    <div style={{width : '100%', height : '160px', marginBottom : '20px'}} >
+                        {/* onClick={() => {navigate('/product/clothes/shoes/' + searchResultList._source.id)}} */}
+                        <div style={{width : '150px', height : '160px', float : 'left'}}>
+                            {/* <img src={product.img_url} style={{maxWidth:'100%', maxHeight:'100%'}}/> */}
+                            <img src={process.env.REACT_APP_AWS_S3_URL + '/product/' + searchResultList._source.barcode + '.jpg'} style={{maxWidth:'90%', maxHeight:'100%'}}/>
+                        </div>
+                        <div style={{height : '160px', textAlign : 'left'}}>
+                            <div style={{paddingRight : '20px', marginBottom : '5px', textOverflow : 'ellipsis', overflow : 'hidden', height:'40px', fontSize : '16px', lineHeight : '20px', WebkitLineClamp : '2', WebkitBoxOrient :'vertical', display:'-webkit-box'}}>{searchResultList._source.title}</div>
+                            <div>
+                                <div style={{fontSize : '15px', color : 'red', float : 'left'}}>와우쿠폰할인가</div>
+                                <div style={{float : 'left'}}>&nbsp;{searchResultList._source.sale_discount_rate}%&nbsp;</div>
+                                <div style={{textDecoration: 'line-through'}}>{searchResultList._source.price}원</div>
+                            </div>
+                            <div style={{marginBottom : '10px'}}>
+                                <div style={{fontSize : '20px', color : 'red'}}>{searchResultList._source.sale_price}원 </div>
+                                {/* <div style={{fontSize : '15px', color : 'red'}}>(10ml당 4,222원)</div> */}
+                            </div>
+                            <div style={{fontSize : '15px', color : 'green'}}>오늘(목) 도착 보장</div>
+                            <div style={{fontSize : '15px'}}>
+                                와우 혜택 적용 <AiOutlineCheck />
+                            </div>
+                            <div>
+                                <div style={{color : 'orange', float : 'left'}}><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /></div>
+                                <div>&nbsp;({searchResultList._source.review_cnt})</div>
+                            </div>
+                        </div>                       
+                    </div>
+                    <hr />
+                    </>
+                )
+            })}
+        </div>
         </>
     )
 }
