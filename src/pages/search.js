c6caf2515f0ad1ffbed0ef0763ddbb39357e2241
jh <jeonghuns0128@naver.com>
2023-03-28 17:57:07 +0900

    자동완성 기능 추가

--- src/pages/search.js
+++ src/pages/search.js
@@ -8,14 +8,23 @@ import { useNavigate } from 'react-router-dom'
 import { HistoryBack } from '../function/function'
 import CloseButton from 'react-bootstrap/CloseButton';
 import axios from 'axios'
-import { searchKeyword, searchList } from '../store/store'
+import { searchKeyword, searchList, searchListAdd } from '../store/store'
 import { AiFillStar, AiOutlineCheck } from 'react-icons/ai'
 import { CgSearchLoading } from 'react-icons/cg'
 
+function AutocompleteArea(){
+    return(
+        <>
+            <div>자동완성 영역</div>
+        </>
+    )
+}
 
 function SearchHeader(){
 
     let [keyword, setKeyword] = useState()
+    let [isOpend, setIsOpend] = useState(false)
+
     //let searchList = useSelector((state)=>{return state.search})
     let dispatch = useDispatch()  
     
@@ -36,13 +45,14 @@ function SearchHeader(){
           </form> */}
 
           <div style={{position: 'relative', color : 'gray', width : '90%', height : '45px', float : 'left', textAlign : 'left', paddingTop : '5px', fontSize : '20px'}}>
-            <input type="text" name="keyword" onChange={(e)=>{setKeyword(e.target.value)}} placeholder='상품이나 스토어를 검색해보세요.' style={{width : '320px', border: "1px solid white", borderRadius: "15px", backgroundColor : 'white', color : 'black', fontSize : '17px'}} />
+            <input type="text" name="keyword" onClick={(e)=>{ setIsOpend(true) }} onChange={(e)=>{setKeyword(e.target.value)}} placeholder='상품이나 스토어를 검색해보세요.' style={{width : '320px', border: "1px solid white", borderRadius: "15px", backgroundColor : 'white', color : 'black', fontSize : '17px'}} />
                 <button onClick={() => {
                     
                     if(keyword === undefined){
                         alert('검색어를 입력해주세요.')
                     }else{
                         axios.get(process.env.REACT_APP_API_URL + '/search/' + keyword).then((result) => {
+                            //console.log(result.data)
                             dispatch(searchList(result.data))
                             dispatch(searchKeyword(keyword))
                             })
@@ -54,6 +64,9 @@ function SearchHeader(){
                     <ImSearch size="22px" style={{marginBottom : '10px'}}/>
                 </button>
           </div>
+          {
+            isOpend === true ? <div>검색창</div> : null
+          }
         </div>
       </>
     )
@@ -62,12 +75,24 @@ function SearchHeader(){
 function SearchMain(){
     
     let searchResult = useSelector((state)=>{return state.search})
-    
+    // console.log("searchResult : " + searchResult)
+    // console.log("searchResult : " + JSON.stringify(searchResult))
+    // console.log(Object.keys(searchResult).length)
+    //console.log(searchResult[0])
+    //console.log(searchResult[0].totcnt)
     return(
       <>
       {
-        ( searchResult == "" || searchResult == null || searchResult == undefined || ( searchResult != null && typeof searchResult == "object" && !Object.keys(searchResult).length )) ? 
-        <SearchEmpty /> : <SearchDetail />
+        (
+            ( searchResult === "" || searchResult === null || searchResult === undefined || ( searchResult != null && typeof searchResult == "object" && !Object.keys(searchResult).length )) 
+            ? <SearchEmpty /> 
+            : ( searchResult[0].statusCode === 200 
+                ? 
+                    ( searchResult !== null && typeof searchResult === "object" && searchResult[0].totcnt === 0 ) 
+                    ? <SearchEmpty /> 
+                    : <SearchDetail /> 
+                : null)
+        )
       }
       </>
     )
@@ -90,6 +115,7 @@ function SearchDetail() {
     let searchResultList = searchResult[0].keyword
     let dispatch = useDispatch()
     let navigate = useNavigate()
+    let [page, setPage] = useState(0)
 
     return(
         <>
@@ -143,6 +169,7 @@ function SearchDetail() {
                         <div style={{width : '150px', height : '160px', float : 'left'}}>
                             {/* <img src={product.img_url} style={{maxWidth:'100%', maxHeight:'100%'}}/> */}
                             <img src={process.env.REACT_APP_AWS_S3_URL + '/product/' + searchResultList._source.barcode + '.jpg'} style={{maxWidth:'90%', maxHeight:'100%'}}/>
+                            {/* <img src={process.env.REACT_APP_AWS_S3_URL + '/product/' + searchResultList._source.barcode + '.jpg'} style={{maxWidth:'90%', maxHeight:'100%', objectFit : 'scale-down'}}/> */}
                         </div>
                         <div style={{height : '160px', textAlign : 'left'}}>
                             <div style={{paddingRight : '20px', marginBottom : '5px', textOverflow : 'ellipsis', overflow : 'hidden', height:'40px', fontSize : '16px', lineHeight : '20px', WebkitLineClamp : '2', WebkitBoxOrient :'vertical', display:'-webkit-box'}}>{searchResultList._source.title}</div>
@@ -170,8 +197,68 @@ function SearchDetail() {
                 )
             })}
         </div>
+        <div>
+            <button onClick={() => {
+                let pageAdd = [...page]
+                pageAdd += 1
+                setPage(pageAdd)
+                console.log("page : " + page)
+                axios.get(process.env.REACT_APP_API_URL + '/search/' + searchKeyword[0] + '?page=' + page).then((result) => {
+                    //console.log(result.data)
+                    dispatch(searchListAdd(result.data))
+                    console.log(result.data)
+                })
+                .catch(() => {
+                    console.log('실패')
+                })
+            
+                // return(
+                //     <div style={{ width : '100%'}}>
+                //       {searchResultList && searchResultList.map(searchResultList => {
+                //             return(
+                //                 <>
+                //                 <div style={{width : '100%', height : '160px', marginBottom : '20px'}} onClick={() => {navigate('/product/' + searchResultList._source.barcode)}} >
+                //                     {/* onClick={() => {navigate('/product/clothes/shoes/' + searchResultList._source.id)}} */}
+                //                     <div style={{width : '150px', height : '160px', float : 'left'}}>
+                //                         {/* <img src={product.img_url} style={{maxWidth:'100%', maxHeight:'100%'}}/> */}
+                //                         <img src={process.env.REACT_APP_AWS_S3_URL + '/product/' + searchResultList._source.barcode + '.jpg'} style={{maxWidth:'90%', maxHeight:'100%'}}/>
+                //                         {/* <img src={process.env.REACT_APP_AWS_S3_URL + '/product/' + searchResultList._source.barcode + '.jpg'} style={{maxWidth:'90%', maxHeight:'100%', objectFit : 'scale-down'}}/> */}
+                //                     </div>
+                //                     <div style={{height : '160px', textAlign : 'left'}}>
+                //                         <div style={{paddingRight : '20px', marginBottom : '5px', textOverflow : 'ellipsis', overflow : 'hidden', height:'40px', fontSize : '16px', lineHeight : '20px', WebkitLineClamp : '2', WebkitBoxOrient :'vertical', display:'-webkit-box'}}>{searchResultList._source.title}</div>
+                //                         <div>
+                //                             <div style={{fontSize : '15px', color : 'red', float : 'left'}}>와우쿠폰할인가</div>
+                //                             <div style={{float : 'left'}}>&nbsp;{searchResultList._source.sale_discount_rate}%&nbsp;</div>
+                //                             <div style={{textDecoration: 'line-through'}}>{searchResultList._source.price}원</div>
+                //                         </div>
+                //                         <div style={{marginBottom : '10px'}}>
+                //                             <div style={{fontSize : '20px', color : 'red'}}>{searchResultList._source.sale_price}원 </div>
+                //                             {/* <div style={{fontSize : '15px', color : 'red'}}>(10ml당 4,222원)</div> */}
+                //                         </div>
+                //                         <div style={{fontSize : '15px', color : 'green'}}>오늘(목) 도착 보장</div>
+                //                         <div style={{fontSize : '15px'}}>
+                //                             와우 혜택 적용 <AiOutlineCheck />
+                //                         </div>
+                //                         <div>
+                //                             <div style={{color : 'orange', float : 'left'}}><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /><AiFillStar /></div>
+                //                             <div>&nbsp;({searchResultList._source.review_cnt})</div>
+                //                         </div>
+                //                     </div>                       
+                //                 </div>
+                //                 <hr />
+                //                 </>
+                //             )
+                //         })}
+                //     </div>
+                // )
+            }}>더보기</button>
+        </div>
         </>
     )
+}
+
+function test(){
+    
 }
 
 export { SearchHeader, SearchMain }
